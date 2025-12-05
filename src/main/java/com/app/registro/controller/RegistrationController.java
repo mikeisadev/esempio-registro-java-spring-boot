73275cf2e88fc1b58b4dcca7a06434d18465c648
Michele <michelework02@gmail.com>
2025-12-05 15:53:41 +0100

    bug fixing: validation RegistrationController:61

--- src/main/java/com/app/registro/controller/RegistrationController.java
+++ src/main/java/com/app/registro/controller/RegistrationController.java
@@ -58,7 +58,7 @@ public class RegistrationController {
         }
 
         if (bindingResult.hasErrors()) {
-            return "pages/register";
+            return "pages/registrati";
         }
 
         User user = new User();
